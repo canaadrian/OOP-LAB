3be133594384eb912fb6fd013c5d24d72205f1c3
canaadrian <cana.adrian.k6b@student.ucv.ro>
2025-11-15 20:59:54 +0200

    Add Results class header file

---
+++ LAB3/pb4/Results.h
@@ -0,0 +1,15 @@
+#ifndef RESULTS_H
+#define RESULTS_H
+
+class Results {
+private:
+    float x1;
+    float x2;
+
+public:
+    Results(float x1, float x2);
+    float getX1() const;
+    float getX2() const;
+};
+
+#endif
